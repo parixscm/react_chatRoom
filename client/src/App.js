b3c0e1d23c98f86ee145678c525a301e8cfdd24a
parixscm <parixscm@gmail.com>
2023-03-02 02:58:04 +0900

    💄 Design: ADD styles to each tag

--- client/src/App.js
+++ client/src/App.js
@@ -7,32 +7,45 @@ const socket = io.connect("http://localhost:3001");
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
     return;
   };
 
   return (
     <div className="App">
-      <h3>Join a Chat</h3>
-      <input
-        type="text"
-        value={username}
-        placeholder="John..."
-        onChange={e => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
-        value={room}
-        placeholder="Room ID"
-        onChange={e => setRoom(e.target.value)}
-      />
-      <button onClick={joinRoom}>Join</button>
-
-      <Chat socket={socket} username={username} room={room} />
+      {!showChat ? (
+        <div className="flex flex-col items-center text-center">
+          <h3 className="text-4xl mt-48 mb-4">Join a Chat</h3>
+          <input
+            type="text"
+            value={username}
+            placeholder="John..."
+            onChange={e => setUsername(e.target.value)}
+            className="m-2 p-1 w-[210px] h-[40px] border-2 border-[#43a047] rounded-md text-lg"
+          />
+          <input
+            type="text"
+            value={room}
+            placeholder="Room ID"
+            onChange={e => setRoom(e.target.value)}
+            className="m-2 p-1 w-[210px] h-[40px] border-2 border-[#43a047] rounded-md text-lg"
+          />
+          <button
+            onClick={joinRoom}
+            className="m-2 p-1 w-56 h-12 bg-[#43a047] rounded-md text-lg cursor-pointer hover:bg-[#2e7d32]"
+          >
+            Join
+          </button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
   );
 }
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -15,33 +16,62 @@ function Chat({ socket, username, room }) {
           new Date(Date.now()).getMinutes(),
       };
       await socket.emit("send_message", messageData);
+      setMessageList(list => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
 
   useEffect(() => {
     socket.on("receive_message", data => {
-      console.log(data);
+      setMessageList(prev => [...prev, data]);
     });
   }, [socket]);
 
   return (
-    <div>
-      {/* Chat header */}
-      <div>
-        <p>Live Chat</p>
+    <div className="mx-auto w-[300px] h-[420px]">
+      <div className="relative w-full h-[45px] bg-gray-200 cursor-pointer">
+        <p className="px-1 py-0 block font-bold">Live Chat</p>
       </div>
-
-      {/* Chat body */}
-
-      {/* Chat footer */}
       <div>
+        {messageList.map(messageContent => {
+          return (
+            <div
+              className={
+                username === messageContent.writer
+                  ? "bg-red-200"
+                  : "bg-blue-200"
+              }
+            >
+              <div>
+                <div>
+                  <p className="text-lg">{messageContent.message}</p>
+                </div>
+                <div className="flex space-x-4">
+                  <p className="text-sm">{messageContent.time}</p>
+                  <p className="text-sm font-bold">{messageContent.writer}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex h-[40px] border-[1px] border-t-0 border-[#263238]">
         <input
           type="text"
           value={currentMessage}
           placeholder="Hey..."
           onChange={e => setCurrentMessage(e.target.value)}
+          onKeyDown={e => {
+            e.key === "Enter" && sendMessage();
+          }}
+          className="px-2 py-0 flex-1 h-full border-0 border-r-[1px] border-r-[#607d8b] outline-none"
         />
-        <button onClick={sendMessage}>&#9658;</button>
+        <button
+          onClick={sendMessage}
+          className="h-full p-2 grid place-items-center border-0 cursor-pointer bg-transparent outline-none text-xl text-gray-200 hover:text-[#43a047]"
+        >
+          &#9658;
+        </button>
       </div>
     </div>
   );
