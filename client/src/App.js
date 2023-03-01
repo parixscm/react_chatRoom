d577345328f1c24a5794812607e36b211c30b16e
parixscm <parixscm@gmail.com>
2023-03-02 00:59:22 +0900

    🎨 Feat: ADD join_room event to client, server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
+
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+    }
+    return;
+  };
+
   return (
     <div className="App">
-      <h3 className="text-red-500">Init project</h3>
+      <h3>Join a Chat</h3>
+      <input
+        type="text"
+        value={username}
+        placeholder="John..."
+        onChange={e => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        value={room}
+        placeholder="Room ID"
+        onChange={e => setRoom(e.target.value)}
+      />
+      <button onClick={joinRoom}>Join</button>
     </div>
   );
 }
